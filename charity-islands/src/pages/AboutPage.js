6f432f5f67713b45427a998c5feb4113ce1cfd8e
Ternel <terneledejerwork@gmail.com>
2022-02-16 22:27:58 -0800

    added lorem ipsum place holder and wakatime

--- charity-islands/src/pages/AboutPage.js
+++ charity-islands/src/pages/AboutPage.js
@@ -4,6 +4,23 @@ function AboutPage() {
   return (
     <div>
       <h1>AboutPage</h1>
+      <div className="border">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus
+        arcu magna, eu blandit leo ultrices non. Sed consequat dapibus erat, ut
+        dapibus neque facilisis vitae. Cras massa tellus, maximus placerat purus
+        eget, consequat auctor enim. Nulla scelerisque dictum odio non rutrum.
+        Proin hendrerit luctus augue, quis vulputate nunc euismod id. Nulla
+        facilisi. Phasellus a leo sit amet velit tincidunt ultrices. Proin
+        scelerisque eleifend tellus ac venenatis. Duis quis tristique metus, et
+        tempus quam. Phasellus elementum vitae massa vitae ullamcorper. Fusce
+        cursus tortor in mi ullamcorper, nec interdum risus consequat.
+      </div>
+      <div className="space">
+        <img
+          src="https://wakatime.com/badge/user/e48fee3e-ead5-429e-880c-36c0316e81d5/project/6ffded4b-aa6b-498d-bc45-f3d16d0c29bf.svg"
+          alt="wakatime"
+        />
+      </div>
     </div>
   );
 }
