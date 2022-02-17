560fdf249f28a426ba36063cd49eceb081d82814
Ternel <terneledejerwork@gmail.com>
2022-02-16 17:42:47 -0800

    add nav and page routes

--- charity-islands/src/App.js
+++ charity-islands/src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
-import Page from "./pages/Page";
-import Page1 from "./pages/Page1";
-import Page2 from "./pages/Page2";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import UserPage from "./pages/UserPage";
+import ErrorPage from "./pages/ErrorPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <div>
-      <h1>Charity Islands</h1>
+      <header>
+        <h1>Charity Islands</h1>
+      </header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/LoginPage">Login</Link>
+        <Link to="/AboutPage">About</Link>
+      </nav>
       <Routes>
-        <Route path="/" element={<Page />} />
-        <Route path="/Page1" element={<Page1 />} />
-        <Route path="/Page2" element={<Page2 />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/UserPage/:username" element={<UserPage />} />
+        <Route path="/AboutPage" element={<AboutPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
