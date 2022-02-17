08d7460e0248eddb425106f8b904675ec97b3571
Ternel <terneledejerwork@gmail.com>
2022-02-16 22:29:25 -0800

    added login routing to userpage

--- charity-islands/src/pages/LoginPage.js
+++ charity-islands/src/pages/LoginPage.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  let navigate = useNavigate();
+  let profile = "/UserPage/";
+  const [name, setName] = useState("");
+
   return (
     <div>
-      <h1>LoginPage</h1>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="userName"
+          placeholder="Username"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="password" placeholder="Password" />
+        <button
+          onClick={() => {
+            profile = profile + name;
+            navigate(profile);
+          }}
+        >
+          Login
+        </button>
+      </form>
     </div>
   );
 }
