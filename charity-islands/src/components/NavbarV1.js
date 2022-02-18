be66e9048c91fbbd08eb77e5253e6dca2e39e709
Ternel <terneledejerwork@gmail.com>
2022-02-17 21:38:38 -0800

    fixed routing bug

--- charity-islands/src/components/NavbarV1.js
+++ charity-islands/src/components/NavbarV1.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
 import HomePage from "../pages/HomePage";
@@ -11,41 +11,34 @@ var brand_light = require("../assets/brand_light.svg");
 function NavbarV1() {
   return (
     <div>
-      <Router>
-        <div>
-          <Navbar bg="dark" variant="dark" expand="sm">
-            <Container>
-              <Navbar.Brand href="/">
-                <img
-                  src={brand_light}
-                  alt="Charity Islands"
-                  width="40"
-                  height="40"
-                />
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link as={Link} to="/">
-                    Home
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/AboutPage">
-                    About
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/LoginPage">
-                    Login
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/AboutPage" element={<AboutPage />} />
-          <Route path="/LoginPage" element={<LoginPage />} />
-        </Routes>
-      </Router>
+      <div>
+        <Navbar bg="dark" variant="dark" expand="sm">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={brand_light}
+                alt="Charity Islands"
+                width="40"
+                height="40"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/AboutPage">
+                  About
+                </Nav.Link>
+                <Nav.Link as={Link} to="/LoginPage">
+                  Login
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     </div>
   );
 }
