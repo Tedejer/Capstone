a76721d0c895b97ccd089a0c8feb4e1ff79c6f9e
Ternel <terneledejerwork@gmail.com>
2022-02-17 21:16:58 -0800

    init bootstrap

--- charity-islands/src/index.js
+++ charity-islands/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
 ReactDOM.render(
