55a490d197c5eadd3f2ad35a0705c62890354fb7
Jericho <perdonjericho@gmail.com>
2022-02-17 17:34:56 -0800

    Updated About Page

--- charity-islands/src/pages/AboutPage.js
+++ charity-islands/src/pages/AboutPage.js
@@ -1,20 +1,45 @@
 import React from "react";
+import "../styling/about.css";
 
 function AboutPage() {
   return (
     <div>
-      <h1>AboutPage</h1>
-      <div className="border">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus
-        arcu magna, eu blandit leo ultrices non. Sed consequat dapibus erat, ut
-        dapibus neque facilisis vitae. Cras massa tellus, maximus placerat purus
-        eget, consequat auctor enim. Nulla scelerisque dictum odio non rutrum.
-        Proin hendrerit luctus augue, quis vulputate nunc euismod id. Nulla
-        facilisi. Phasellus a leo sit amet velit tincidunt ultrices. Proin
-        scelerisque eleifend tellus ac venenatis. Duis quis tristique metus, et
-        tempus quam. Phasellus elementum vitae massa vitae ullamcorper. Fusce
-        cursus tortor in mi ullamcorper, nec interdum risus consequat.
-      </div>
+      <h1 class="About-Header">AboutPage</h1>
+      <section class="About=container">
+        <div class="Question-1">
+          <h1 class="About-Page">What is Charity Islands?</h1>
+
+          <div class="About-Body">
+            <h3>
+              Charity Islands is a site that aims to help a person easily find
+              and determine what charity foundation they would like to donate to
+              or what form of community service they would like to participate
+              in.
+            </h3>
+          </div>
+        </div>
+
+        <div class="Question-2">
+          <h1 class="About-Page">Can Charity Islands be used nationwide?</h1>
+
+          <div class="About-Body">
+            <h3>No, Charity Islands is based in Ventura County only.</h3>
+          </div>
+        </div>
+
+        <div class="Question-3">
+          <h1 class="About-Page">How was Charity Islands created?</h1>
+
+          <div class="About-Body">
+            <h3>
+              Charity Islands came from the idea that people can be selfish. The
+              site would help those people feel less self-centered by giving
+              them opportunities of good deeds.
+            </h3>
+          </div>
+        </div>
+      </section>
+
       <div className="space">
         <img
           src="https://wakatime.com/badge/user/e48fee3e-ead5-429e-880c-36c0316e81d5/project/6ffded4b-aa6b-498d-bc45-f3d16d0c29bf.svg"
