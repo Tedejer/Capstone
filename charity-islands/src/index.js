8c7785b2304d6a8a904ea05b05bfc310ba2b192d
Jericho <perdonjericho@gmail.com>
2022-02-19 15:47:16 -0800

    added cancel button in register page

--- charity-islands/src/index.js
+++ charity-islands/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <BrowserRouter>
