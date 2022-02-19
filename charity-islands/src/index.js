951c35d2607b6c3ac408059faf531de72be73f25
Ternel <terneledejerwork@gmail.com>
2022-02-19 15:45:34 -0800

    remove unecessary bootrap import

--- charity-islands/src/index.js
+++ charity-islands/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,5 +11,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-
