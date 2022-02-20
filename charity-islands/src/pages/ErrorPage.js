878cf86e0f92ba1af4162f6222ccc427df2f0880
Jericho <perdonjericho@gmail.com>
2022-02-19 22:06:46 -0800

    updated error page

--- charity-islands/src/pages/ErrorPage.js
+++ charity-islands/src/pages/ErrorPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
   return (
-    <div>
+    <div class="error-msg">
       <h1>Error: Page not found :(</h1>
+        <Link as={Link} to="/">
+          <button class="btn btn-primary error-home-button" type="button">
+            Back to Home
+          </button>
+        </Link>
     </div>
   );
 }
