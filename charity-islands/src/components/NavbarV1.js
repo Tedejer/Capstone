813690ea557baa1a1d9e2f80a4198d7cfa9f81d6
JeffreyCastillejo <57466618+JeffreyCastillejo@users.noreply.github.com>
2022-02-19 16:11:05 -0800

    fixed navbar

--- charity-islands/src/components/NavbarV1.js
+++ charity-islands/src/components/NavbarV1.js
@@ -8,13 +8,14 @@ function NavbarV1() {
     <div>
       <div>
         <Navbar bg="dark" variant="dark" expand="sm">
-          <Container>
+          <Navbar className="Container-fluid">
             <Navbar.Brand as={Link} to="/">
               <img src={logo} alt="Charity Islands" width="40" height="40" />
             </Navbar.Brand>
+            </Navbar>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
+              <Nav className="ms-auto">
                 <Nav.Link as={Link} to="/LoginPage">
                   Login
                 </Nav.Link>
@@ -26,7 +27,6 @@ function NavbarV1() {
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
-          </Container>
         </Navbar>
       </div>
     </div>
