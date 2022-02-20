cf6b989e1b34489f746f29019608c2e36fae9904
Ternel <terneledejerwork@gmail.com>
2022-02-19 22:40:13 -0800

    fix overflow

--- charity-islands/src/pages/UserPage.js
+++ charity-islands/src/pages/UserPage.js
@@ -6,23 +6,28 @@ function UserPage() {
   return (
     <div className="profile-body">
       <h1 class="welcome-msg"> Welcome to Charity Islands</h1>
-      <img class="profile_pic" src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/45434/134438/main-image"/>
-      <h1 class="username_text">{username}</h1>
-      <h3 class="email_text">{username}@email.com</h3>
-      <h1 class="profile_points">Points:</h1>
+      <img
+        class="profile_pic"
+        src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/45434/134438/main-image"
+      />
+      <div className="container">
+        <h1 class="username_text">{username}</h1>
+        <h3 class="email_text">{username}@email.com</h3>
+        <h1 class="profile_points">Points:</h1>
+      </div>
 
-      <table class="history_table">
-      <tr>
-        <th>Group: </th>
-        <th>Location: </th>
-        <th>Type: </th>
-        <th>Amount Donated: </th>
-        <th>Hours worked: </th>
-      </tr>
-    </table>
-
+      <div className="">
+        <table class="history_table">
+          <tr>
+            <th>Group: </th>
+            <th>Location: </th>
+            <th>Type: </th>
+            <th>Amount Donated: </th>
+            <th>Hours worked: </th>
+          </tr>
+        </table>
+      </div>
     </div>
-  
   );
 }
 export default UserPage;
