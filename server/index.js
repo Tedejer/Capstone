6fddb76c6037e09acc16027e0b459a3942a62dc9
Ternel <terneledejerwork@gmail.com>
2022-02-23 13:30:30 -0800

    deploy

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 // server/index.js
-
+const path = require("path");
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-const app = express();
-
+// Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Server Status: Connection Estabished ✔️" });
+  res.json({ message: "Hello from server!" });
 });
 
-app.listen(PORT, () => {
-  console.log('Welcome to the Charity Islands Backend')
-  console.log(`Server listening on ${PORT}`);
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
