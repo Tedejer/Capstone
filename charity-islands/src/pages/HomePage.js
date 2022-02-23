9f002d3639b358a5a4200f4a5050d179da04e88f
Jericho <perdonjericho@gmail.com>
2022-02-22 21:42:44 -0800

    Deleted comments

--- charity-islands/src/pages/HomePage.js
+++ charity-islands/src/pages/HomePage.js
@@ -6,26 +6,6 @@ import AdvSearch from "../components/AdvSearch.js";
 
 function HomePage() {
   return (
-    // <div class="Home-Page">
-    //   <div class="Logo">
-    //     <img src={logo} alt="Charity Islands" width="120" height="120" />
-    //     <span class="Home-Page-Title">Charity Islands</span>
-    //   </div>
-    //   <div class="Search-Bar">
-    //     <form class="d-flex">
-    //       <input
-    //         class="form-control me-2"
-    //         type="search"
-    //         placeholder="Search"
-    //         aria-label="Search"
-    //       />
-    //       <button class="btn btn-outline-primary" type="submit">
-    //         Search
-    //       </button>
-    //     </form>
-    //   </div>
-    //   <AdvSearch />
-    // </div>
     <div class="home-body">
       <div className="container-home">
           <div class="Logo">
