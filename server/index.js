4cfff3dad6b0dc62227034493b2612c1ce03e4ca
Ternel <terneledejerwork@gmail.com>
2022-02-23 13:31:56 -0800

    fix folder path for build

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const express = require("express");
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../charity-islands/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -12,5 +12,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(
+    path.resolve(__dirname, "../charity-islands/build", "index.html")
+  );
 });
