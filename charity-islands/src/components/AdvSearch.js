0df010537b6074ba4f1580857dbc3738ee9c190e
Ternel <terneledejerwork@gmail.com>
2022-02-25 06:06:31 -0800

    Adjusted pos inside adv option

--- charity-islands/src/components/AdvSearch.js
+++ charity-islands/src/components/AdvSearch.js
@@ -5,9 +5,11 @@ import {
   Form,
   FormControl,
   FormCheck,
+  FormLabel,
   FormText,
-  Col,
+  Container,
   Row,
+  Col,
 } from "react-bootstrap";
 
 function AdvSearch() {
@@ -26,24 +28,37 @@ function AdvSearch() {
       </Button>
       <Collapse in={open}>
         <div className="">
-          <Form>
+          <Container fluid="sm">
+            <FormLabel>Category:</FormLabel>
             <Row>
-              <FormText>Type</FormText>
               <Col>
                 <Form.Check type="checkbox" label="Environment" />
+              </Col>
+              <Col>
                 <Form.Check type="checkbox" label="Food Bank" />
-                <Form.Check type="checkbox" label="Homeless" />
-                <Form.Check type="checkbox" label="Animal" />
               </Col>
             </Row>
             <Row>
-              <FormText>Time</FormText>
               <Col>
-                <Form.Check type="checkbox" label="Morning" />
-                <Form.Check type="checkbox" label="Afternoon" />
+                <Form.Check type="checkbox" label="Homeless" />
+              </Col>
+              <Col>
+                <Form.Check type="checkbox" label="Health" />
               </Col>
             </Row>
-          </Form>
+            <Form>
+              <FormLabel>Time:</FormLabel>
+              <Form.Control
+                className="timeBox"
+                size="sm"
+                placeholder="eg. 5:30pm"
+                type="time"
+              ></Form.Control>
+              <Form.Text id="passwordHelpBlock" muted>
+                For example 3:00 PM
+              </Form.Text>
+            </Form>
+          </Container>
         </div>
       </Collapse>
     </div>
