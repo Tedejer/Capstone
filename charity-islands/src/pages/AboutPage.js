809b74adfe95a728289c1366a09b6ded517d99ba
Ternel <terneledejerwork@gmail.com>
2022-02-28 15:02:28 -0800

    adjusted about page layout & add  about component

--- charity-islands/src/pages/AboutPage.js
+++ charity-islands/src/pages/AboutPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
+// Components
+import AboutDes from "../components/AboutDes";
+
 function AboutPage() {
   return (
     <div>
@@ -10,40 +13,46 @@ function AboutPage() {
             <h1>About</h1>
           </Col>
         </Row>
-
-        <Row className="about-pad">
+        <Row>
           <Col>
-            <h4>What is Charity Islands?</h4>
-          </Col>
-          <Col md="6">
-            <p>
-              Charity Islands is a site that aims to help a person easily find
-              and determine what charity foundation they would like to donate to
-              or what form of community service they would like to participate
-              in.
-            </p>
-          </Col>
-        </Row>
+            <Row className="about-pad">
+              <Col>
+                <h4>What is Charity Islands?</h4>
+              </Col>
+              <Col md="6">
+                <p>
+                  Charity Islands is a site that aims to help a person easily
+                  find and determine what charity foundation they would like to
+                  donate to or what form of community service they would like to
+                  participate in.
+                </p>
+              </Col>
+            </Row>
 
-        <Row className="about-pad">
-          <Col>
-            <h4>Can Charity Islands be used nationwide?</h4>
-          </Col>
-          <Col md="6">
-            <p>No, Charity Islands is based in Ventura County only.</p>
-          </Col>
-        </Row>
+            <Row className="about-pad">
+              <Col>
+                <h4>Can Charity Islands be used nationwide?</h4>
+              </Col>
+              <Col md="6">
+                <p>No, Charity Islands is based in Ventura County only.</p>
+              </Col>
+            </Row>
 
-        <Row className="about-pad">
-          <Col>
-            <h4>How was Charity Islands created?</h4>
+            <Row className="about-pad">
+              <Col>
+                <h4>How was Charity Islands created?</h4>
+              </Col>
+              <Col md="6">
+                <p>
+                  Charity Islands came from the idea that people can be selfish.
+                  The site would help those people feel less self-centered by
+                  giving them opportunities of good deeds.
+                </p>
+              </Col>
+            </Row>
           </Col>
-          <Col md="6">
-            <p>
-              Charity Islands came from the idea that people can be selfish. The
-              site would help those people feel less self-centered by giving
-              them opportunities of good deeds.
-            </p>
+          <Col>
+            <AboutDes />
           </Col>
         </Row>
       </Container>
