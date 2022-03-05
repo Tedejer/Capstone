4fc5f2028fd1e7a7132186e0e7c26a8fc0a483ab
JeffreyCastillejo <57466618+JeffreyCastillejo@users.noreply.github.com>
2022-03-04 19:46:25 -0800

    InfoPage updates

--- charity-islands/src/pages/InfoPage.js
+++ charity-islands/src/pages/InfoPage.js
@@ -8,29 +8,32 @@ function InfoPage() {
     <div className="info-body">
       <div className="container">
         <div class="info-header">
-          Search Results with keyword, "{charityname}"
+          <h1>{charityname}</h1>
         </div>
-
-        <div class="charity-desc">
-          Description
-          <div class="desc-content">
-            <h1>Who runs this charity?</h1>
-            <LoremIpsum p={1} />
-            <h1>Number of hours offered:</h1>
-            <LoremIpsum p={1} />
-            <h1>Where does my money go?</h1>
-            <LoremIpsum p={1} />
-            <h1>Location:</h1>
-            <LoremIpsum p={1} />
-          </div>
+        <hr></hr>
+        <div class="desc-who">
+          <h1>Who runs this charity?</h1>
+          <LoremIpsum p={1} />
         </div>
-
-        <div class="charity-relevant">News related to {charityname}</div>
-        {/* <div class="also-viewed">Also Viewed</div> */}
-
+        <div class=" desc-type">
+          <h1>What type of charity is this?</h1>
+          <LoremIpsum p={1} />
+        </div>
+        <div class=" desc-hours">
+          <h1>Number of hours offered</h1>
+          <LoremIpsum p={1} />
+        </div>
+        <div class="desc-where">
+          <h1>Where does my money go?</h1>
+          <LoremIpsum p={1} />
+        </div>
+        <div class="desc-location">
+          <h1>Location</h1>
+          <LoremIpsum p={2} />
+        </div>
         <div class="charity-reviews">
-          Reviews
-          <LoremIpsum p={10} />
+          <h1>Reviews</h1>
+          <LoremIpsum p={2} />
         </div>
 
         {/* <div class="metric-display">Metric Display</div> */}
